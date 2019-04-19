4fc0eb2bd4613d37a3260d57e213dea988427662
Michael Felipe <michaelfelipe.ti@gmail.com>
2019-04-18 21:37:37 -0300

    Adição da funcionalidade Update de cursos e sua respesctiva tela.

---
+++ src/main/java/br/com/senac/CursoController.java
@@ -0,0 +1,68 @@
+package br.com.senac;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.senac.dominio.Curso;
+import br.com.senac.servico.CategoriaService;
+import br.com.senac.servico.CursoService;
+import br.com.senac.servico.exception.ObjectNotFoundException;
+
+@Controller
+@RequestMapping("/curso")
+public class CursoController {
+	
+	@Autowired
+	private CursoService cursoService;
+	
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	@GetMapping("/listarCurso")
+	public ModelAndView listaCursos() {
+		ModelAndView mv = new ModelAndView("curso/paginaCursos");
+		mv.addObject("cursos", cursoService.listaCursos());
+		return mv;
+	}
+	
+	@GetMapping("/adicionarCurso")
+	public ModelAndView add() {
+		ModelAndView mv = new ModelAndView("curso/paginaAdicionarCurso");
+		mv.addObject("curso", new Curso());
+		mv.addObject("categorias", categoriaService.listaCategorias());
+		return mv;
+	}
+	
+	@PostMapping("/salvarCurso")
+	public ModelAndView inserir(Curso curso) {
+		cursoService.inserir(curso);
+		return listaCursos();
+	}
+	
+	@GetMapping("/excluirCurso/{id}")
+	public ModelAndView delete(@PathVariable("id") Integer id) {
+		ModelAndView mv = new ModelAndView("curso/paginaAlterar");
+		mv.addObject("curso", cursoService.buscar(id));
+		mv.addObject("categorias", categoriaService.listaCategorias());
+		return mv;
+	}
+	
+	@GetMapping("/alterarCurso/{id}")
+	public ModelAndView alterar(@PathVariable("id") Integer id) throws ObjectNotFoundException {
+		ModelAndView mv = new ModelAndView("curso/paginaAlterar");
+		mv.addObject("curso", cursoService.buscar(id));
+		mv.addObject("categorias", categoriaService.listaCategorias());
+		return mv;
+	}
+	
+	@PostMapping("/alterarCurso")
+	public ModelAndView alterar(Curso curso) {
+		cursoService.alterar(curso);
+		return listaCursos();
+	}
+}
---
+++ src/main/java/br/com/senac/servico/CursoService.java
@@ -0,0 +1,49 @@
+package br.com.senac.servico;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.senac.dominio.Curso;
+import br.com.senac.repositorio.CursoRepositorio;
+import br.com.senac.servico.exception.ObjectNotFoundException;
+
+@Service
+public class CursoService {
+	
+	@Autowired
+	private CursoRepositorio repoCurso;
+	
+	public Curso buscar(Integer id) throws ObjectNotFoundException {
+		Optional<Curso> objCurso = repoCurso.findById(id);
+		return objCurso.orElseThrow(() -> new ObjectNotFoundException
+				("Curso nao encontrado! id:" + id + ", Tipo: " + Curso.class.getName())); 
+	}
+	
+	public Curso inserir(Curso curso) {
+		// Adicionando objeto novo com id null, pois o id vai ser gerado na persistencia.
+		curso.setId(null);
+		return repoCurso.save(curso);
+	}
+	
+	
+	public Curso alterar(Curso objCurso) throws ObjectNotFoundException {
+		Curso objCursoEncontrado = buscar(objCurso.getId());
+		objCursoEncontrado.setNome(objCurso.getNome());
+		objCursoEncontrado.setDescricao(objCurso.getDescricao());
+		objCursoEncontrado.setPreco(objCurso.getPreco());
+		objCursoEncontrado.setCategorias(objCurso.getCategorias());
+		
+		return repoCurso.save(objCursoEncontrado);
+	}
+	
+	public void excluir(Integer id) {
+		repoCurso.deleteById(id);
+	}
+	
+	public List<Curso> listaCursos(){
+		return repoCurso.findAll();
+	}
+}
